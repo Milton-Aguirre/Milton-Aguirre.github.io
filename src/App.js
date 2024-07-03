8152c1286f8aff8aff3c3be1a4594a287db7bcec
Milton-Aguirre <aguirremilton0@gmail.com>
2024-07-03 11:32:20 -0300

    fix: corrigida versão para celular

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ function App() {
         <p>Formação em Matemática</p>
       </header>
       <nav style={styles.nav}>
+        <a href="#coment">Calendario de Eventos</a>
         <a href="#links">Links</a>
         <a href="#portfolio">Portfólio</a>
         <a href="#material">Material</a>
-        <a href="#coment">Comentários</a>
       </nav>
       <div style={styles.container}>
         <div style={styles.main}>
 
-          <div style={styles.leftColumn}>
+          {/* <div style={styles.leftColumn}>
 
             <h2>Calendário de Eventos</h2>
             <p>A seguir encontrará uma lista de eventos matemáticos que serão realizados nos próximos meses.</p>
@@ -29,9 +29,9 @@ function App() {
             <h2>Bolsas de Estudos</h2>
             <p>Os links a seguir são de processos seletivos para cursos de pós-graduação em matemática</p>
             <p>**Próximamente terá links atualizados de editais de bolsas**</p>
-          </div>
+          </div> */}
 
-          <div style={styles.rightColumn}>
+          <div>{/* c>*/}
 
             <section style={styles.section}>
               <h2 style={styles.sectionTitle}>Sobre Mim</h2>
@@ -54,11 +54,25 @@ function App() {
               </div>
             </section>
 
+            <section>
+              <div style={styles.leftColumnEvents}>
+                <h2 style={styles.sectionTitle}>Calendário de Eventos</h2>
+                  <p>A seguir encontrará uma lista de eventos matemáticos que serão realizados nos próximos meses.</p>
+                  <ul style={styles.iconsCalendar}>
+                    <li><a className="link" href="https://sites.google.com/view/icmam2024/home" target="_blank" rel="noopener noreferrer">Congreso ICMAM Latin América 2024</a></li>
+                    <li><a className="link" href="https://sites.google.com/view/isaac-icmam-conference-4-women/p%C3%A1gina-principal" target="_blank" rel="noopener noreferrer">ISAACS-ICMAM Conference for Women in Mathematics 2024</a></li>
+                  </ul>
+              </div>
+              <div style={styles.rightColumnEvents}>
+                <p>imagem</p>
+              </div>
+            </section>
+
             <section id="links" style={styles.section}>
               <h2 style={styles.sectionTitle}>Links</h2>
               <div style={styles.icons}>
                 <a className="link" href="https://www.linkedin.com/in/milton-aguirre-mat-52300751/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                <a className="link" href="https://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=K8950240D2&tokenCaptchar=03AFcWeA7z4Qjl4qRQ4aBCqex6x1B9zSPzeZDg7-hAyKBGx3gLsqbRDLy4Z4Uj5urBINBzuenKqMHaovqVT4zbcLQHNrnYYPwgdsS12pyyaF1bi4SdsTmjrlU7mIv0HXR5iThkezDRcuF5A9UteSsQeeWnO8EqC36dX4BdpWMs75lW41VdZh2Y7BtJ6Ydzs9WMBZBHUaRnwj9ekWgjjIVyPVdICYTkvYpLvCSmBs67O-2-2TX1iQIkSgPoROLkvUycMDk3MO-RD369EkHjzroKdOnEPc-LKNk85dYTvAK76jG9l5FYZWtK7RNZTWyAm3Nb5HvEcAlSVvm-FavyBkKSR7KAHQLzPqk-8dSzaVcfqXKm8q_dJ7FmkUfWxluCMnhPOhDDiSEWD_ALX3jIjPFkTrIuWtPHP42WoPuhdH8UpZ9X9QqTZSoW4BqJWmaE3dWcX8Bjc7jJA6zj3K6VvYxVaM6Omhk7Ibr-nDwH3ZpnymspUF4J5qnEg6hif9Xmejb6sA0kjO7xObEzKb_zDm9NBoavtNPV2ps6k377PLn4xoDiQTXRlZmIwck1YBwx6sCwtkVgQdRW8JMxyDLCdt8Quy6eZ80dYjbOjiXFkYlQGQGfrdw08Ru4L0B9FIUrp7AAvGOXMZuVy-Op6IT1WD0PKrQUCxZUHo9zdg" target="_blank" rel="noopener noreferrer">Currículo Lattes</a>
+                <a className="link" href="https://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=K8950240D2&tokenCaptchar=03AFcWeA7z4Qjl4qRQ4aBCqex6x1B9zSPzeZDg7-hAyKBGx3gLsqbRDLy4Z4Uj5urBINBzuenKqMHaovqVT4zbcLQHNrnYYPwgdsS12pyyaF1bi4SdsTmjrlU7mIv0HXR5iThkezDRcuF5A9UteSsQeeWnO8EqC36dX4BdpWMs75lW41VdZh2Y7BtJ6Ydzs9WMBZBHUaRnwj9ekWgjjIVyPVdICYTkvYpLvCSmBs67O-2-2TX1iQIkSgPoROLkvUycMDk3MO-RD369EkHjzroKdOnEPc-LKNk85dYTvAK76jG9l5FYZWtK7RNZTWyAm3Nb5HvEcAlSVvm-FavyBkKSR7KAHQLzPqk-8dSzaVcfqXKm8q_dJ7FmkUfWxluCMnhPOhDDiSEWD_ALX3jIjPFkTrIuWtPHP42WoPuhdH8UpZ9X9QqTZSoW4BqJWmaE3dWcX8Bjc7jJA6zj3K6VvYxVaM6Omhk7Ibr-nDwH3ZpnymspUF4J5qnEg6hif9Xmejb6sA0kjO7xObEzKb_zDm9NBoavtNPV2ps6k377PLn4xoDiQTXRlZmIwck1YBwx6sCwtkVgQdRW8JMxyDLCdt8Quy6eZ80dYjbOjiXFkYlQGQGfrdw08Ru4L0B9FIUrp7AAvGOXMZuVy-Op6IT1WD0PKrQUCxZUHo9zdg" target="_blank" rel="noopener noreferrer">CV Lattes</a>
                 <a className="link" href="https://github.com/Milton-Aguirre" target="_blank" rel="noopener noreferrer">GitHub</a>
                 <a className="link" href="https://sites.google.com/view/icmamlatinamerica/home" target="_blank" rel="noopener noreferrer">ICMAM Latin América</a>
              </div>
@@ -96,22 +110,39 @@ function App() {
 
             <section id="material">
               <h2 style={styles.sectionTitle}>Material</h2>
-                <div style={styles.nColumnSection}>
-                  <div style={styles.project}>
-                    <h3>Cálculo 1</h3>
-                    <p>Proximamente PDF com exercicios resolvidos</p>
+                <div>
+                  <div style={styles.nColumnSection}>
+                    <div style={styles.project}>
+                      <h3>Cálculo 1</h3>
+                      <p>Proximamente PDF com exercicios resolvidos</p>
+                    </div>
+                    <div style={styles.project}>
+                      <h3>Cálculo 2</h3>
+                      <p>Proximamente PDF com exercicios resolvidos</p>
+                    </div>
+                    <div style={styles.project}>
+                      <h3>Cálculo 3</h3>
+                      <p>Proximamente PDF com exercicios resolvidos</p>
+                    </div>
                   </div>
-                  <div style={styles.project}>
-                    <h3>Cálculo 2</h3>
-                    <p>Proximamente PDF com exercicios resolvidos</p>
-                  </div>
-                  <div style={styles.project}>
-                    <h3>Cálculo 3</h3>
-                    <p>Proximamente PDF com exercicios resolvidos</p>
-                  </div>
-                  <div style={styles.project}>
-                    <h3>Álgebra Linear</h3>
-                    <p>Proximamente PDF com exercicios resolvidos</p>
+                </div>
+                <div>
+                  <p> </p>
+                </div>
+                <div>
+                  <div style={styles.nColumnSection}>
+                    <div style={styles.project}>
+                      <h3>Álgebra Linear</h3>
+                      <p>Proximamente PDF com exercicios resolvidos</p>
+                    </div>
+                    <div style={styles.project}>
+                      <h3>Análisis Numérico</h3>
+                      <p>Proximamente PDF com exercicios resolvidos</p>
+                    </div>
+                    <div style={styles.project}>
+                      <h3>Teoría de Gráfos</h3>
+                      <p>Proximamente PDF com exercicios resolvidos</p>
+                    </div>
                   </div>
                 </div>
             </section>
@@ -179,10 +210,10 @@ const styles = {
     flex: '25%',
     backgroundColor: '#C70039',
     color: '#fff',
-    padding: '1em',
+    padding: '0.5em',
     width: '100%',
     boxSizing: 'border-box',
-    margin: '1em',
+    margin: '0.5sem',
   },
   section: {
     marginBottom: '2em',
@@ -197,15 +228,37 @@ const styles = {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'flex-start',
+    width: '100%',
   },
+  // '@media (max-width: 600px)': { // Define estilos específicos para telas pequenas (celulares)
+  //   nColumnSection: {
+  //     flexDirection: 'column', // Muda a direção do flex para coluna
+  //     justifyContent: 'center', // Centraliza os itens na coluna
+  //   },
+  // },
   leftColumn: {
-    flex: '1', //Ocupa uma quarta parte da distribuição horizontal da coluna
-    marginRight: '10px',
+    //flex: '1', //Ocupa uma quarta parte da distribuição horizontal da coluna
+    //marginRight: '10px',
     backgroundColor: '#FBE7E5', // Opcional: fundo cinza para a coluna da esquerda
-    padding: '10px',
+    //padding: '10px',
+    width: '100%',
   },
   rightColumn: {
-    flex: '3', //Ocupa três quartas partes da distribuição horizontal da coluna
+    // flex: '3', //Ocupa três quartas partes da distribuição horizontal da coluna
+    // marginRight: '10px',
+    // marginLeft: '10px',
+    backgroundColor: '#f0f0f0', // Opcional: fundo cinza muito claro para a coluna da direita
+    // padding: '10px',
+    width: '100%',
+  },
+  leftColumnEvents: {
+    flex: '3', //Ocupa uma quarta parte da distribuição horizontal da coluna
+    marginRight: '10px',
+    backgroundColor: '#7B8DA3', // Opcional: fundo cinza para a coluna da esquerda
+    padding: '10px',
+  },
+  rightColumnEvents: {
+    flex: '1', //Ocupa três quartas partes da distribuição horizontal da coluna
     marginRight: '10px',
     marginLeft: '10px',
     backgroundColor: '#f0f0f0', // Opcional: fundo cinza muito claro para a coluna da direita
@@ -232,7 +285,7 @@ const styles = {
   project: {
     flex: '1',
     margin: '0 10px',
-    padding: '10px',
+    padding: '5px',
     backgroundColor: '#d3d3d3',
     borderRadius: '5px',
   },
