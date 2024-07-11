b6c76282cda0ddb20dd0c16d954cba1e0d07c68d
Milton-Aguirre <aguirremilton0@gmail.com>
2024-07-11 16:06:14 -0300

    criado um componente novo ´material´

---
+++ src/components/material.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Material = () => {
+  return (
+    <div>
+        <div className="nColumnSection">
+            <div className="project">
+                <h3>Sistemas Dinâmicos</h3>
+                <p>Proximamente PDF com exercicios resolvidos</p>
+            </div>
+            <div className="project">
+                <h3>Teoria de Galois</h3>
+                <p>Proximamente PDF com exercicios resolvidos</p>
+            </div>
+            <div className="project">
+                <h3>Formas Diferenciais</h3>
+                <p>Proximamente PDF com exercicios resolvidos</p>
+            </div>
+        </div>
+        <div className="nColumnSection">
+            <div className="project">
+                <h3>Geometria Riemanniana</h3>
+                <p>Proximamente PDF com exercicios resolvidos</p>
+            </div>
+            <div className="project">
+                <h3>Análisis Numérico</h3>
+                <p>Proximamente PDF com exercicios resolvidos</p>
+            </div>
+            <div className="project">
+                <h3>Teoría de Gráfos</h3>
+                <p>Proximamente PDF com exercicios resolvidos</p>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Material;
