3d92c02873231bc22633f9ffe2273583fce9eb11
Milton-Aguirre <aguirremilton0@gmail.com>
2024-08-08 17:02:22 -0300

    fix: modificada a section portfolio

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
+import { FaFacebook, FaInstagram, FaYoutube, FaGithub, FaLinkedin } from 'react-icons/fa';
 import Contact from './components/contacts';
 import Material from './components/material';
+import Portfolio from './components/portfolio';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <p>Formação em Matemática</p>
       </header>
       <nav className="nav">
-        <a href="#calendar">Eventos</a>
+        <a href="#events">Eventos</a>
         <a href="#links">Links</a>
         <a href="#portfolio">Portfólio</a>
         <a href="#material">Material</a>
@@ -42,20 +43,34 @@ function App() {
               </div>
             </section>
 
-            <section id="calendar">
+            <section id="events">
               <h2 className="sectionTitle">Eventos</h2>
-              <div className="nColumnSection">
-                <div className="leftColumnEvents">
-                  <p>A seguir encontrará uma lista de eventos matemáticos que serão realizados nos próximos meses.</p>
-                  <ul className="iconsCalendar">
-                    <li><a className="link" href="https://sites.google.com/view/icmam2024/home" target="_blank" rel="noopener noreferrer">Congreso ICMAM Latin América 2024</a></li>
-                    <li><a className="link" href="https://sites.google.com/view/isaac-icmam-conference-4-women/p%C3%A1gina-principal" target="_blank" rel="noopener noreferrer">ISAACS-ICMAM Conference for Women in Mathematics 2024</a></li>
-                  </ul>
+              <h2 className='subSectionTitle'>Próximos</h2>
+                <div className="nColumnSection">
+                  <div className="leftColumnEvents">
+                    <p>A seguir encontrará uma lista de eventos matemáticos que serão realizados nos próximos meses.</p>
+                    <ul className="iconsEvents">
+                      <li><a className="link" href="https://sites.google.com/view/icmam2024/home" target="_blank" rel="noopener noreferrer">Congreso ICMAM Latin América 2024</a></li>
+                      <li><a className="link" href="https://sites.google.com/view/isaac-icmam-conference-4-women/p%C3%A1gina-principal" target="_blank" rel="noopener noreferrer">ISAACS-ICMAM Conference for Women in Mathematics 2024</a></li>
+                    </ul>
+                  </div>
+                  <div className="rightColumnEvents">
+                    <img src={`${process.env.PUBLIC_URL}/images/KleinBottle.png`} alt="Klein bottle" className="imageBottle" />
+                  </div>
                 </div>
-                <div className="rightColumnEvents">
-                  <img src={`${process.env.PUBLIC_URL}/images/KleinBottle.png`} alt="Klein bottle" className="imageBottle" />
+              <h2 className='subSectionTitle'>Histórico</h2>
+                <div className="nColumnSection">
+                  <div className="leftColumnEvents">
+                    <p>Histórico de eventos que foram realizados nos últios anos.</p>
+                    <ul className="iconsEvents">
+                      <li><a className="link" href="https://sites.google.com/view/math08conference" target="_blank" rel="noopener noreferrer">Congreso ICMAM Latin América 2020</a></li>
+                      <li><a className="link" href="https://sites.google.com/view/matematicasunivalleicmam2022/home" target="_blank" rel="noopener noreferrer">Congreso ICMAM Latin América 2022</a></li>
+                    </ul>
+                  </div>
+                  <div className="rightColumnEvents">
+                    <img src={`${process.env.PUBLIC_URL}/images/KleinBottle.png`} alt="Klein bottle" className="imageBottle" />
+                  </div>
                 </div>
-              </div>
             </section>
 
             <section id="links" className="section">
@@ -70,34 +85,8 @@ function App() {
 
             <section id="portfolio">
               <h2 className="sectionTitle">Portfólio de Projetos</h2>
-              <div className="nColumnSection">
-                <div className="project">
-                  <h3>TCC Bacharel</h3>
-                  <h5>Universidad del Valle</h5>
-                  <p>Álgebras de Frobenius</p>
-                  <a href="/pdfs/projeto_de_grado.pdf" target="_blank" rel="noopener noreferrer">
-                    <img src={`${process.env.PUBLIC_URL}/images/logo-PDF-1.png`} alt="PDF" className="iconPDF" />
-                  </a>
-                </div>
-                <div className="project">
-                  <h3>Disertação Mestrado</h3>
-                  <h5>Universidade Federal de Viçosa</h5>
-                  <p>Alguns resultados da teoria de Rk -ações sobre variedades diferenciáveis</p>
-                  <a href="/pdfs/disertação_mestrado.pdf" target="_blank" rel="noopener noreferrer">
-                    <img src={`${process.env.PUBLIC_URL}/images/logo-PDF-1.png`} alt="PDF" className="iconPDF" />
-                  </a>
-                </div>
-                <div className="project">
-                  <h3>TCC Especialização</h3>
-                  <h5>Univesp</h5>
-                  <p>Perfil dos alunos da Universidade Virtual do Estado de São Paulo em relação à inclusão digital, 2022</p>
-                  <a href="/pdfs/tcc_especializacao.pdf" target="_blank" rel="noopener noreferrer">
-                    <img src={`${process.env.PUBLIC_URL}/images/logo-PDF-1.png`} alt="PDF" className="iconPDF" />
-                  </a>
-                </div>
-              </div>
+              <Portfolio />
             </section>
-
             <section id="material">
               <h2 className="sectionTitle">Material</h2>
               <Material />
@@ -134,6 +123,12 @@ function App() {
               <a href="https://www.youtube.com/@conferenciainternacionalma6791" target="_blank" rel="noopener noreferrer">
                 <FaYoutube size={40} className="socialIcon" />
               </a>
+              <a href="https://github.com/Milton-Aguirre" target="_blank" rel="noopener noreferrer">
+                <FaGithub size={40} className="socialIcon" />
+              </a>
+              <a href="https://www.linkedin.com/in/milton-aguirre-mat-52300751/" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin size={40} className="socialIcon" />
+              </a>
             </div>
         </aside>
         <p>&copy; 2024 Site Pessoal. Todos os direitos reservados.</p>
@@ -143,153 +138,3 @@ function App() {
 }
 
 export default App;
-
-
-//             <h2>Calendário de Eventos</h2>
-//             <p>A seguir encontrará uma lista de eventos matemáticos que serão realizados nos próximos meses.</p>
-//             <ul style={styles.iconsCalendar}>
-//               <li><a className="link" href="https://sites.google.com/view/icmam2024/home" target="_blank" rel="noopener noreferrer">Congreso ICMAM Latin América 2024</a></li>
-//               <li><a className="link" href="https://sites.google.com/view/isaac-icmam-conference-4-women/p%C3%A1gina-principal" target="_blank" rel="noopener noreferrer">ISAACS-ICMAM Conference for Women in Mathematics 2024</a></li>
-//             </ul>
-
-//             <h2>Bolsas de Estudos</h2>
-//             <p>Os links a seguir são de processos seletivos para cursos de pós-graduação em matemática</p>
-//             <p>**Próximamente terá links atualizados de editais de bolsas**</p>
-//           </div> */}
-
-//           <div>
-
-//             <section style={styles.section}>
-//               <h2 style={styles.sectionTitle}>Biografia</h2>
-//               <div style={styles.nColumnSection}>
-//                 <div style={styles.imageColumn}>
-//                   <img src={`${process.env.PUBLIC_URL}/images/Milton.jpeg`} alt="Personal Photo" style={styles.image} />
-//                 </div>
-//                 <div style={styles.textColumn}>
-//                   <p>
-//                     <strong>Milton Aguirre</strong> é formado em Matemática e possui mestrado na mesma área, 
-//                     além de atuar como cientista de dados. Com um sólido background técnico, 
-//                     também possui o título de Técnico em Instalações Elétricas. Além disso, 
-//                     é especializado em Ensino de Educação a Distância, combinando conhecimentos 
-//                     acadêmicos com experiência prática em tecnologia e educação. Seu trabalho é 
-//                     marcado pela aplicação de métodos matemáticos avançados para resolver 
-//                     problemas complexos e pela paixão por compartilhar conhecimento em sala de aula e através de 
-//                     plataformas digitais.
-//                   </p>
-//                 </div>
-//               </div>
-//             </section>
-
-//             <section id="calendar">
-//               <h2 style={styles.sectionTitle}>Eventos</h2>
-//                 <div style={styles.nColumnSection}>
-//                   <div style={styles.leftColumnEvents}>
-//                       <p>A seguir encontrará uma lista de eventos matemáticos que serão realizados nos próximos meses.</p>
-//                       <ul style={styles.iconsCalendar}>
-//                         <li><a className="link" href="https://sites.google.com/view/icmam2024/home" target="_blank" rel="noopener noreferrer">Congreso ICMAM Latin América 2024</a></li>
-//                         <li><a className="link" href="https://sites.google.com/view/isaac-icmam-conference-4-women/p%C3%A1gina-principal" target="_blank" rel="noopener noreferrer">ISAACS-ICMAM Conference for Women in Mathematics 2024</a></li>
-//                       </ul>
-//                   </div>
-//                   <div style={styles.rightColumnEvents}>
-//                     <img src={`${process.env.PUBLIC_URL}/images/KleinBottle.png`} alt="Klein bottle" style={styles.imageBottle} />
-//                   </div>
-//                 </div>
-//             </section>
-
-//             <section id="links" style={styles.section}>
-//               <h2 style={styles.sectionTitle}>Links</h2>
-//               <div style={styles.icons}>
-//                 <a className="link" href="https://www.linkedin.com/in/milton-aguirre-mat-52300751/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-//                 <a className="link" href="https://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=K8950240D2&tokenCaptchar=03AFcWeA7z4Qjl4qRQ4aBCqex6x1B9zSPzeZDg7-hAyKBGx3gLsqbRDLy4Z4Uj5urBINBzuenKqMHaovqVT4zbcLQHNrnYYPwgdsS12pyyaF1bi4SdsTmjrlU7mIv0HXR5iThkezDRcuF5A9UteSsQeeWnO8EqC36dX4BdpWMs75lW41VdZh2Y7BtJ6Ydzs9WMBZBHUaRnwj9ekWgjjIVyPVdICYTkvYpLvCSmBs67O-2-2TX1iQIkSgPoROLkvUycMDk3MO-RD369EkHjzroKdOnEPc-LKNk85dYTvAK76jG9l5FYZWtK7RNZTWyAm3Nb5HvEcAlSVvm-FavyBkKSR7KAHQLzPqk-8dSzaVcfqXKm8q_dJ7FmkUfWxluCMnhPOhDDiSEWD_ALX3jIjPFkTrIuWtPHP42WoPuhdH8UpZ9X9QqTZSoW4BqJWmaE3dWcX8Bjc7jJA6zj3K6VvYxVaM6Omhk7Ibr-nDwH3ZpnymspUF4J5qnEg6hif9Xmejb6sA0kjO7xObEzKb_zDm9NBoavtNPV2ps6k377PLn4xoDiQTXRlZmIwck1YBwx6sCwtkVgQdRW8JMxyDLCdt8Quy6eZ80dYjbOjiXFkYlQGQGfrdw08Ru4L0B9FIUrp7AAvGOXMZuVy-Op6IT1WD0PKrQUCxZUHo9zdg" target="_blank" rel="noopener noreferrer">CV Lattes</a>
-//                 <a className="link" href="https://github.com/Milton-Aguirre" target="_blank" rel="noopener noreferrer">GitHub</a>
-//                 <a className="link" href="https://sites.google.com/view/icmamlatinamerica/home" target="_blank" rel="noopener noreferrer">ICMAM Latin América</a>
-//              </div>
-//             </section>
-
-//             <section id="portfolio">
-//               <h2 style={styles.sectionTitle}>Portfólio de Projetos</h2>
-//                 <div style={styles.nColumnSection}>
-//                   <div style={styles.project}>
-//                     <h3>TCC Bacharel</h3>
-//                     <h5>Universidad del Valle</h5>
-//                     <p>Álgebras de Frobenius</p>
-//                     <a href="/pdfs/projeto_de_grado.pdf" target="_blank" rel="noopener noreferrer">
-//                       <img src={`${process.env.PUBLIC_URL}/images/logo-PDF-1.png`} alt="PDF" style={styles.iconPDF} />
-//                     </a>
-//                   </div>
-//                   <div style={styles.project}>
-//                     <h3>Disertação Mestrado</h3>
-//                     <h5>Universidade Federal de Viçosa</h5>
-//                     <p>Alguns resultados da teoria de Rk -ações sobre variedades diferenciáveis</p>
-//                     <a href="/pdfs/disertação_mestrado.pdf" target="_blank" rel="noopener noreferrer">
-//                       <img src={`${process.env.PUBLIC_URL}/images/logo-PDF-1.png`} alt="PDF" style={styles.iconPDF} />
-//                     </a>
-//                   </div>
-//                   <div style={styles.project}>
-//                     <h3>TCC Especialização</h3>
-//                     <h5>Univesp</h5>
-//                     <p>Descrição do projeto 3...</p>
-//                     <a href="/pdfs/disertação_mestrado.pdf" target="_blank" rel="noopener noreferrer">
-//                       {/* <img src="/images/logo-PDF-1.png" alt="PDF" style={styles.iconPDF} /> */}
-//                     </a>
-//                   </div>
-//                 </div>
-//             </section>
-
-//             <section id="material">
-//               <h2 style={styles.sectionTitle}>Material</h2>
-
-//             </section>
-
-//             {/* <section>
-//               <h2 style={styles.sectionTitle}>Comentários</h2>
-//               <form style={styles.form}>
-//                 <input type="text" placeholder="Seu nome" style={styles.input} />
-//                 <input type="email" placeholder="Seu email" style={styles.input} />
-//                 <textarea placeholder="Seu comentário" style={styles.textarea}></textarea>
-//                 <button type="submit" style={styles.button}>Enviar</button>
-//               </form>
-//             </section> */}
-
-//             <section>
-//               <h2 style={styles.sectionTitle}>Comentários</h2>
-//                 <Contact/>
-//             </section>
-
-//             <section>
-//               <h2 style={styles.sectionTitle}>Visitantes</h2>
-//               {/* <div style={styles.imgContainer}> */}
-//               <a href="https://info.flagcounter.com/vHkJ">
-//                 <img 
-//                   src="https://s01.flagcounter.com/countxl/vHkJ/bg_FFFFFF/txt_000000/border_000000/columns_4/maxflags_12/viewers_3/labels_0/pageviews_0/flags_0/percent_1/" 
-//                   alt="Flag Counter" 
-//                   border="0"
-//                   style={styles.imgStyle}
-//                 />
-//               </a>
-//               {/* </div> */}
-//             </section>
-//           </div>
-//         </div>
-
-//         <aside style={styles.sidebar}>
-//           <div style={styles.socialMedia}>
-//               <a href="https://www.facebook.com/cofradia.academica.1" target="_blank" rel="noopener noreferrer">
-//                 <FaFacebook size={40} style={styles.socialIcon} />
-//               </a>
-//               <a href="https://www.instagram.com/icmam_math_conference?igsh=eWx2M3FlYWxucTJ0" target="_blank" rel="noopener noreferrer">
-//                 <FaInstagram size={40} style={styles.socialIcon} />
-//               </a>
-//               <a href="https://www.youtube.com/@conferenciainternacionalma6791" target="_blank" rel="noopener noreferrer">
-//                 <FaYoutube size={40} style={styles.socialIcon} />
-//               </a>
-//             </div>
-//         </aside>
-
-//       </div>
-//       <footer style={styles.footer}>
-//         <p>&copy; 2024 Site Pessoal. Todos os direitos reservados.</p>
-//       </footer>
-//     </div>
-//   );
-// }
